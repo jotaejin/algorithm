a7a9945d365ad23925e82139aaa31da02e2560be
jotaejin <ad0362320@naver.com>
2024-04-11 10:53:59 +0900

    commit log

--- src/dfs활용/P동전활용.java
+++ src/dfs활용/P동전활용.java
@@ -1,4 +1,22 @@
 package dfs활용;
 
+import java.util.Scanner;
+
 public class P동전활용 {
+    static int n,m,answer = Integer.MAX_VALUE;
+    public static void dfs(int l, int sum, int[] arr){
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            m = sc.nextInt();//동전 종류
+        }
+
+        dfs(0,0,arr);
+        System.out.println(answer);//동전의 최소개수
+    }
 }
